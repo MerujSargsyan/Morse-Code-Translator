723a383f4efdcdfabc5b6731e88f4a2d8ae60d75
MeruzhanS <meruzhansargsyan48@gmail.com>
2024-01-16 21:03:24 -0800

    fixed user input

--- Main.java
+++ Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Parser p = new Parser();
 
+        if(args.length != 2) {
+            System.out.println("Incorrect usage, type " + "-h help" + 
+                " for help");
+            return;
+        }
+
         if("-m".equals(args[0])) {
             System.out.println(p.convertTextToMorse(args[1]));
         } else if("-d".equals(args[0])) {
@@ -13,6 +19,17 @@ public class Main {
             }
         } else if("-t".equals(args[0])) {
             System.out.println(p.convertMorseToText(args[1]));
+        } else if("-h".equals(args[0])) { 
+            System.out.println("To translate from text to morse: " + 
+                "-m <Text>");
+            System.out.println("To translate from morse to text: " + 
+                "-t <MorseCode>");
+            System.out.println("To see a visual representation of a word in " +
+                "morse code: -d <Text>");
+        } else {
+            System.out.println("Incorrect usage, type -h for help");
         }
     }
 }
+
+// TODO: fix incorrect input for -t and -d and -m and so on...
