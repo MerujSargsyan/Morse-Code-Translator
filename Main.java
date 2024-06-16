3f58c8f0a8da535378598860c86a7bb4d3b80977
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-06-16 09:11:57 -0700

    adding gui for user input

--- Main.java
+++ Main.java
@@ -1,14 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        String[] input = openInputWindow();
         Parser p = new Parser();
 
+        /*if(inputs[0].equals("-h")) {
+            printHelp();
+        }
+
         if(args.length != 2) {
-            System.out.println("Incorrect usage, type " + " '-h help' " + 
+            System.out.println("Incorrect usage, type " + " '-h' " + 
                 " for help");
             return;
         }
 
-        if("-m".equals(args[0])) {
+        if("-morse".equals(args[0])) {
             System.out.println(p.convertTextToMorse(args[1]));
         } else if("-d".equals(args[0])) {
             Display d = new Display();
@@ -17,19 +25,42 @@ public class Main {
             } catch(Exception ex) {
                 ex.printStackTrace();
             }
-        } else if("-t".equals(args[0])) {
+        } else if("-text".equals(args[0])) {
             System.out.println(p.convertMorseToText(args[1]));
-        } else if("-h".equals(args[0])) { 
-            System.out.println("To translate from text to morse: " + 
-                "-m <Text>");
-            System.out.println("To translate from morse to text: " + 
-                "-t <MorseCode>");
-            System.out.println("To see a visual representation of text in " +
-                "morse code: -d <Text>");
         } else {
             System.out.println("Incorrect usage, type -h for help");
-        }
+        }*/
+    }
+
+    public static void printHelp() {
+        System.out.println("To translate from text to morse: " + 
+             "-m <Text>");
+         System.out.println("To translate from morse to text: " + 
+             "-t <MorseCode>");
+         System.out.println("To see a visual representation of text in " +
+            "morse code: -d <Text>");
+    }
+
+    public static String[] openInputWindow() {
+        JFrame window = new JFrame("Morse Input");
+        window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+        window.setSize(500, 500);
+        window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.PAGE_AXIS));
+
+        JComboBox jcb = new JComboBox(new String[]{"-morse", "-text"});
+
+        JTextArea jta = new JTextArea(5, 20);
+        jta.setLineWrap(true);
+        jta.setWrapStyleWord(true);
+
+        JButton jb = new JButton("Translate");
+
+        window.add(jcb);
+        window.add(jta);
+        window.add(jb);
+        window.setVisible(true);
+
+        return new String[]{"a", "b"};
     }
 }
 
-// TODO: fix incorrect input for -t and -d and -m and so on...
